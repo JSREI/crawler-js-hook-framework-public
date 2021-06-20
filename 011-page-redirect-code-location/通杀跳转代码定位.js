e65e9ffcc1ef4c854395586087d1b3687ef94372
CC11001100 <cc11001100@qq.com>
2021-06-21 03:17:06 +0800

    添加跳转逻辑定位通杀方案

--- 011-page-redirect-code-location/通杀跳转代码定位.js
+++ 011-page-redirect-code-location/通杀跳转代码定位.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         页面跳转阻断
-// @namespace    https://github.com/CC11001100
+// @namespace    https://github.com/CC11001100/crawler-js-hook-framework-public/tree/master/011-page-redirect-code-location
 // @version      0.1
 // @description  阻断页面跳转，留在当前页面分析
 // @author       CC11001100
@@ -9,6 +9,9 @@
 // @grant        none
 // ==/UserScript==
 (() => {
+
+    // 使用说明： https://github.com/CC11001100/crawler-js-hook-framework-public/tree/master/011-page-redirect-code-location
+
     window.onbeforeunload = () => {
         debugger;
         return false;
