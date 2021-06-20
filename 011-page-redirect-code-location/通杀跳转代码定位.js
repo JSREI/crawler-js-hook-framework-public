b09da3b757478f6e270d49cef8a5d450fce59f3d
CC11001100 <cc11001100@qq.com>
2021-06-21 03:15:18 +0800

    添加跳转逻辑定位通杀方案

---
+++ 011-page-redirect-code-location/通杀跳转代码定位.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         页面跳转阻断
+// @namespace    https://github.com/CC11001100
+// @version      0.1
+// @description  阻断页面跳转，留在当前页面分析
+// @author       CC11001100
+// @match       *://*/*
+// @run-at      document-start
+// @grant        none
+// ==/UserScript==
+(() => {
+    window.onbeforeunload = () => {
+        debugger;
+        return false;
+    }
+})();
